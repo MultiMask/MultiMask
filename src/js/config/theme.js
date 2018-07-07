b60a990bd06fbadc25ab9aec01a372a969c4d258
Leonid Semenov <kalesin90@gmail.com>
2018-07-07 15:11:24 +0300

    Improvement markup

--- src/js/config/theme.js
+++ src/js/config/theme.js
@@ -17,11 +17,9 @@ injectGlobal`
     margin: 0;
     padding: 0;
     width: 100%;
-    height: 100%;
   }
 
   #app-container {
     width: 100%;
-    height: 100%;
   }
 `;
--- src/js/popup/layouts/AuthLayout.jsx
+++ src/js/popup/layouts/AuthLayout.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import styled from 'react-emotion';
-import logo from '../../../img/logo.png';
-import label from '../../../img/label.svg';
+import logo from '../../../img/logo.svg';
 import Typography from '../ui/Typography';
 
 const Container = styled.div`
-  width: 450px;
-  height: 600px;
-  padding: 0 55px;
+  width: 350px;
+  padding: 55px;
   box-sizing: border-box;
 `;
 
@@ -15,21 +13,20 @@ const Header = styled.header`
   display: flex;
   align-items: center;
   text-align: center;
-  padding-top: ${props => (props.login ? '100px' : '50px')};
+  padding-top: ${props => (props.login ? '50px' : 0)};
   flex-direction: column;
 `;
 
 const Image = styled('img')`
-  margin-top: 16px};
+  width: 90px;
 `;
 
 const AuthLayout = ({ login, children }) => (
   <Container>
     <Header>
       <Image alt="logo" src={logo} />
-      <Image alt="label" src={label} />
       <Typography color="main" variant="title">
-        {login ? 'Login to your acccount' : 'Create new account'}
+        MultiMask
       </Typography>
     </Header>
     {children}
