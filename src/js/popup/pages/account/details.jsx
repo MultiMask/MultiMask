db69d18c2820549bff5ad5724d8ec2a3e1e82b45
Eugene Schemelev <schemelev.eugene@gmail.com>
2018-07-09 01:57:30 +0300

    mm-26 re-write wallets list

--- src/js/popup/pages/account/details.jsx
+++ src/js/popup/pages/account/details.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import networkImg from '../../../helpers/networkImg';
 import networkSign from '../../../helpers/networkSign';
 import accountActions from '../../actions/account';
 import stateActions from '../../actions/state';
+import Icon from '../../ui/components/Icon';
 import { getCurrentWallet } from './../../select';
 import TXS from './txs';
 
@@ -35,7 +35,8 @@ class AccountInfo extends React.Component {
 
   get image() {
     const account = this.props.account;
-    return <img src={networkImg(account)} />;
+
+    return account.blockchain ? <Icon type={account.blockchain} size="s" /> : null;
   }
 
   get balance() {
--- src/js/popup/pages/account/list/index.jsx
+++ src/js/popup/pages/account/list/index.jsx
@@ -2,23 +2,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import stateActions from '../../../actions/state';
+import Icon from '../../../ui/components/Icon';
 import Item from './item';
 
 class AccountList extends React.Component {
   get items() {
     if (this.props.accounts && this.props.accounts.length > 0) {
-      return this.props.accounts.map(account => {
-        return <Item account={account} key={account.name} />;
-      });
+      let total = { balance: 0 };
+
+      return (
+        <div className="Wallets">
+          <div className="Wallets-Items">
+            {this.props.accounts.map(account => {
+              total.balance += account.info.balance;
+
+              return (
+                <div key={account.name} className="Wallets-Item">
+                  <Item account={account} />
+                </div>
+              );
+            })}
+          </div>
+          <div className="Wallets-Total">
+            <div className="Wallets-Label">Total:</div>
+            <div className="Wallets-Value">
+              {total.balance}
+              <br />BTC
+            </div>
+            <div className="Wallets-Label">
+              ?<br />USD
+            </div>
+          </div>
+        </div>
+      );
     }
 
     return (
-      <div className="balance__empty">
-        <div className="title">
-          <span>No walletes</span>
+      <div className="NoWallets">
+        <div className="NoWallets-Icon">
+          <Icon className="Icon" type="no-wallets" size="xl" />
         </div>
-        <div className="action">
-          <div onClick={this.props.createWallet}>Create new</div>
+        <div className="NoWallets-Title">
+          <span>No wallets</span>
+        </div>
+        <div className="NoWallets-Actions">
+          <a className="Link" onClick={this.props.createWallet}>
+            Create new
+          </a>
         </div>
       </div>
     );
