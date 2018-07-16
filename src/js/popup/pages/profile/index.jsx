0b0ee032da2ad67841b4332c26a4cd0975c187b0
mikeshukshin <merqrylive@gmail.com>
2018-07-16 19:48:45 +0300

    add simple ux actions for profiles

--- src/js/popup/pages/profile/index.jsx
+++ src/js/popup/pages/profile/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import styled from 'react-emotion';
+import Typography from '../../ui/Typography';
+
+import FA from 'react-fontawesome';
 
 import profileActions from './../../actions/profile';
 
@@ -14,12 +17,37 @@ class Profiles extends React.Component {
     const { list } = this.props;
 
     return list.map(profile => {
-      return <Item key={profile.id}>{profile.name}</Item>;
+      return (
+        <Item key={profile.id}>
+          <Label>{profile.name}</Label>
+          <Actions>
+            <Icon name="edit" />
+            <Icon name="upload" />
+            <Icon name="trash" />
+          </Actions>
+        </Item>
+      );
     });
   }
 
   render() {
-    return <Wrapper>{this.list}</Wrapper>;
+    return (
+      <Wrapper>
+        <List>{this.list}</List>
+        <Bottom>
+          <Button>
+            <Typography color="main" align="center">
+              Add
+            </Typography>
+          </Button>
+          <Button>
+            <Typography color="main" align="center">
+              Import
+            </Typography>
+          </Button>
+        </Bottom>
+      </Wrapper>
+    );
   }
 }
 
@@ -31,8 +59,37 @@ export default connect(
 )(Profiles);
 
 const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const List = styled.div`
+  height: calc(100% - 36px);
   padding: 20px 10px;
 `;
+
+const Bottom = styled.div`
+  height: 36px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Button = styled.div`
+  flex-grow: 1;
+  text-align: center;
+  cursor: pointer;
+`;
+
 const Item = styled.div`
   padding: 5px;
+`;
+
+const Label = styled.span`
+  width: 70%;
+  display: inline-block;
+`;
+const Actions = styled.span``;
+const Icon = styled(FA)`
+  margin-left: 7px;
+  font-size: 120%;
+  cursor: pointer;
 `;
