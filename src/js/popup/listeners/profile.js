7f68b9c46d31d7bf143a6a990cb9985769b54f71
Leonid Semenov <kalesin90@gmail.com>
2018-07-30 22:39:32 +0300

    Add loading accounts after change current profile

--- src/js/popup/listeners/profile.js
+++ src/js/popup/listeners/profile.js
@@ -3,6 +3,7 @@ import { downloadFile } from '../helpers';
 import { PROFILE_GETLIST_RESULT, PROFILE_EXPORT_RESULT, PROFILE_SELECT_RESULT } from './../../constants/profile';
 
 import profileActions from './../actions/profile';
+import accountActions from './../actions/account';
 
 export default function({ dispatch, getState }) {
   messaging.on(PROFILE_GETLIST_RESULT, ({ list, profileId }) => {
@@ -15,5 +16,7 @@ export default function({ dispatch, getState }) {
 
   messaging.on(PROFILE_SELECT_RESULT, ({ profileId }) => {
     profileActions.getSelectResult(profileId)(dispatch, getState);
+    //TODO: Make middleware for chain async actions
+    accountActions.getInfo()(dispatch, getState);
   });
 }
