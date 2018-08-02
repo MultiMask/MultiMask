550df461e6a82105d153532825abfd52987709d5
mikeshukshin <merqrylive@gmail.com>
2018-08-02 18:50:59 +0300

    Move TX to InternalMessage

--- src/js/background/listeners/tx.js
+++ src/js/background/listeners/tx.js
@@ -4,25 +4,27 @@ import { TX_CREATE, TX_PAYMENT, TX_PAYMENT_GET, TX_PAYMENT_RESULT } from './../.
 
 let tx = null;
 
-export default ({ messaging, App }) => {
-  // Create TX from UI
-  messaging.on(TX_CREATE, ({ id, tx }) => {
-    App.io.tx.sendTX({ id, tx });
-    // const account = App.accountManager.getAccount(name);
-    // account.sendTX(tx);
-  });
+export default ({ App }) => ({ type, payload }, sendResponse) => {
+  switch (type) {
+    // Create TX from UI
+    case TX_CREATE: {
+      const { id, tx } = payload;
+      App.io.tx.sendTX({ id, tx });
+      break;
+    }
+  }
 
   // Payment Create TX
-  messaging.on(TX_PAYMENT, data => {
-    tx = data;
-    createWindow({});
-  });
+  // messaging.on(TX_PAYMENT, data => {
+  //   tx = data;
+  //   createWindow({});
+  // });
 
   // Return TX into popup to payment
-  messaging.on(TX_PAYMENT_GET, data => {
-    messaging.send({
-      type: TX_PAYMENT_RESULT,
-      payload: tx
-    });
-  });
+  // messaging.on(TX_PAYMENT_GET, data => {
+  //   messaging.send({
+  //     type: TX_PAYMENT_RESULT,
+  //     payload: tx
+  //   });
+  // });
 };
--- src/js/popup/actions/tx.js
+++ src/js/popup/actions/tx.js
@@ -1,16 +1,10 @@
-import messaging from '../message';
+import InternalMessage from '../../libs/InternalMessage';
 
 import { TX_CREATE } from './../../constants/tx';
 
 const StateActions = {
   createTx: ({ id, tx }) => (dispatch, getState) => {
-    messaging.send({
-      type: TX_CREATE,
-      payload: {
-        id,
-        tx
-      }
-    });
+    return InternalMessage.payload(TX_CREATE, { id, tx }).send();
   }
 };
 export default StateActions;
