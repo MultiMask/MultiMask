8f1f8b67d0fd416143ddf982abe447ee598436c5
Leonid Semenov <kalesin90@gmail.com>
2018-09-04 17:56:24 +0300

    Add prettier-tslint package

--- src/js/popup/pages/wallet/SelectStep.tsx
+++ src/js/popup/pages/wallet/SelectStep.tsx
@@ -3,36 +3,36 @@ import FormLayout from './FormLayout';
 import Select from '../../ui/Select';
 
 class SelectStep extends React.Component<any, any> {
-  constructor(props) {
-    super(props);
-    const { options, selectedValue } = this.props;
-    const item = selectedValue ? selectedValue : options[0];
-    this.state = {
-      selectedItem: item
-    };
-  }
+	constructor(props) {
+		super(props);
+		const { options, selectedValue } = this.props;
+		const item = selectedValue ? selectedValue : options[0];
+		this.state = {
+			selectedItem: item
+		};
+	}
 
-  componentDidMount() {
-    const { options, selectedValue, onChange } = this.props;
-    if (!selectedValue) {
-      onChange(options[0]);
-    }
-  }
+	public componentDidMount() {
+		const { options, selectedValue, onChange } = this.props;
+		if (!selectedValue) {
+			onChange(options[0]);
+		}
+	}
 
-  handleChange = event => {
-    this.setState({ selectedItem: event });
-    this.props.onChange(event);
-  };
+	public handleChange = event => {
+		this.setState({ selectedItem: event });
+		this.props.onChange(event);
+	};
 
-  render() {
-    const { options, onChange, ...props } = this.props;
-    const { selectedItem } = this.state;
-    return (
-      <FormLayout {...props}>
-        <Select options={options} value={selectedItem} onChange={this.handleChange} />
-      </FormLayout>
-    );
-  }
+	public render() {
+		const { options, onChange, ...props } = this.props;
+		const { selectedItem } = this.state;
+		return (
+			<FormLayout {...props}>
+				<Select options={options} value={selectedItem} onChange={this.handleChange} />
+			</FormLayout>
+		);
+	}
 }
 
 export default SelectStep;
--- src/js/popup/reducers/auth.ts
+++ src/js/popup/reducers/auth.ts
@@ -1,34 +1,34 @@
 import { AUTH_LOGIN_FAIL, AUTH_CHECK_SUCCESS, AUTH_CHECK_FAIL, AUTH_LOGOUT_SUCCESS } from '../../constants/auth';
 
 const initialState = {
-  isAuth: false,
-  error: false
+	isAuth: false,
+	error: false
 };
 
 export default function accountReducer(state = initialState, action) {
-  switch (action.type) {
-    case AUTH_LOGIN_FAIL:
-      return {
-        ...state,
-        error: true
-      };
-    case AUTH_CHECK_FAIL:
-      return {
-        ...state,
-        hasPass: action.hasPass,
-        check: false
-      };
-    case AUTH_CHECK_SUCCESS:
-      return {
-        ...state,
-        check: true
-      };
-    case AUTH_LOGOUT_SUCCESS:
-      return {
-        ...state,
-        isAuth: false
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case AUTH_LOGIN_FAIL:
+			return {
+				...state,
+				error: true
+			};
+		case AUTH_CHECK_FAIL:
+			return {
+				...state,
+				hasPass: action.hasPass,
+				check: false
+			};
+		case AUTH_CHECK_SUCCESS:
+			return {
+				...state,
+				check: true
+			};
+		case AUTH_LOGOUT_SUCCESS:
+			return {
+				...state,
+				isAuth: false
+			};
+		default:
+			return state;
+	}
 }
