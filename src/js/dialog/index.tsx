488aa4813ab74fdd6e5b27b9de666a5d8901e171
mikeshukshin <merqrylive@gmail.com>
2018-09-12 13:42:36 +0300

    Show price in Dialog window on send tx

--- src/js/dialog/index.tsx
+++ src/js/dialog/index.tsx
@@ -8,6 +8,7 @@ import InternalMessage from '../libs/InternalMessage';
 import { AUTH_IS_READY } from '../constants/auth';
 import { ACCOUNT_INFO } from '../constants/account';
 import { TX_PAYMENT_GET, TX_APPROVE_RESULT } from '../constants/tx';
+import { SETTINGS_LOAD_CURRENCY_PRICE } from '../constants/settings';
 
 import DialogLayout from '../popup/layouts/DialogLayout';
 import Typography from '../popup/ui/Typography';
@@ -26,246 +27,279 @@ const Actions = styled('div')`
 `;
 
 interface IAppState {
-	isLoaded?: boolean;
-	isReady?: boolean;
+  isLoaded?: boolean;
+  isReady?: boolean;
 
-	tx?: any;
-	txId?: string;
-	blockchain?: string;
+  tx?: any;
+  txId?: string;
+  blockchain?: string;
 
-	accounts?: any[];
-	account?: any;
-	selectValue?: ISelectOption;
+  accounts?: any[];
+  account?: any;
+  selectValue?: ISelectOption;
+
+  prices?: any;
 }
 
 interface ISelectOption {
-	value: string;
-	label: string;
+  value: string;
+  label: string;
 }
 
 export default class App extends React.Component<{}, IAppState> {
-	public state = {
-		isLoaded: false,
-		isReady: false,
-		tx: null,
-		txId: null,
-		blockchain: null,
-		accounts: [],
-		account: null,
-		selectValue: null
-	};
+  public state = {
+    isLoaded: false,
+    isReady: false,
+    tx: null,
+    txId: null,
+    blockchain: null,
+    accounts: [],
+    account: null,
+    selectValue: null,
+    prices: null,
+  };
 
-	public componentDidMount() {
-		InternalMessage.signal(AUTH_IS_READY)
-			.send()
-			.then(({ isReady }) => {
-				if (!isReady) throw new Error('You need to authorize');
+  public componentDidMount() {
+    InternalMessage.signal(AUTH_IS_READY)
+      .send()
+      .then(({ isReady }) => {
+        if (!isReady) throw new Error('You need to authorize');
 
-				return InternalMessage.signal(ACCOUNT_INFO).send();
-			})
-			.then(accounts => {
-				this.setAccounts(accounts);
+        return InternalMessage.signal(ACCOUNT_INFO).send();
+      })
+      .then(accounts => {
+        this.setAccounts(accounts);
 
-				return InternalMessage.signal(TX_PAYMENT_GET).send();
-			})
-			.then( payload => {
-				this.setTxInfo(payload);
-			})
-			.catch(e => {
-				console.log(e);
-				this.setState(state => ({
-					...state,
-					isLoaded: true,
-					isReady: false
-				}));
-			});
-	}
+        return InternalMessage.signal(TX_PAYMENT_GET).send();
+      })
+      .then( payload => {
+        this.setTxInfo(payload);
 
-	public setTxInfo(payload: any) {
-		const accounts = this.state.accounts.filter(acc => acc.blockchain === payload.data.blockchain);
-		const account = payload.data.tx.from
-			? accounts.find(acc => acc.info.address === payload.data.tx.from)
-			: accounts[0];
+        return InternalMessage.signal(SETTINGS_LOAD_CURRENCY_PRICE)
+          .send()
+      })
+      .then(({ payload: { prices } }) => {
+        this.setState({prices});
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState(state => ({
+          ...state,
+          isLoaded: true,
+          isReady: false
+        }));
+      });
+  }
 
-		console.log('receive tx > ', payload);
+  public setTxInfo(payload: any) {
+    const accounts = this.state.accounts.filter(acc => acc.blockchain === payload.data.blockchain);
+    const account = payload.data.tx.from
+      ? accounts.find(acc => acc.info.address === payload.data.tx.from)
+      : accounts[0];
 
-		this.setState(state => ({
-			...state,
-			tx: payload.data.tx,
-			txId: payload.id,
-			blockchain: payload.data.blockchain,
-			accounts,
-			account,
-			selectValue: this.getOption(account)
-		}));
-	}
+    console.log('receive tx > ', payload);
 
-	public setAccounts(accounts) {
-		let account;
-		if (accounts && accounts.length > 0) {
-			account = accounts[0];
-		}
+    this.setState(state => ({
+      ...state,
+      tx: payload.data.tx,
+      txId: payload.id,
+      blockchain: payload.data.blockchain,
+      accounts,
+      account,
+      selectValue: this.getOption(account)
+    }));
+  }
 
-		this.setState(state => ({
-			...state,
-			accounts,
-			account,
-			selectValue: this.getOption(account),
-			isLoaded: true,
-			isReady: true
-		}));
-	}
+  public setAccounts(accounts) {
+    let account;
+    if (accounts && accounts.length > 0) {
+      account = accounts[0];
+    }
 
-	public getOption = (account): ISelectOption => {
-		return { value: account.id, label: `${account.info.address} - ${account.info.balance} ${account.blockchain}` };
-	};
+    this.setState(state => ({
+      ...state,
+      accounts,
+      account,
+      selectValue: this.getOption(account),
+      isLoaded: true,
+      isReady: true
+    }));
+  }
 
-	public handleChooseAccount = e => {
-		const { accounts } = this.state;
-		this.setState({ account: accounts.find(account => account.id === e.value), selectValue: e });
-	};
+  public getOption = (account): ISelectOption => {
+    return { value: account.id, label: `${account.info.address} - ${account.info.balance} ${account.blockchain}` };
+  };
 
-	public onSubmit = payload => {
-		const { tx, txId } = this.state;
+  public handleChooseAccount = e => {
+    const { accounts } = this.state;
+    this.setState({ account: accounts.find(account => account.id === e.value), selectValue: e });
+  };
 
-		InternalMessage.payload(TX_APPROVE_RESULT, {
-			id: txId,
-			tx
-		})
-			.send()
-			.then(() => {
-				// TODO: show txhash and loading to update info about it
-				window.close();
-			});
-	};
+  public onSubmit = payload => {
+    const { tx, txId } = this.state;
 
-	public onReject = () => {
-		window.close();
-	};
+    InternalMessage.payload(TX_APPROVE_RESULT, {
+      id: txId,
+      tx
+    })
+      .send()
+      .then(() => {
+        // TODO: show txhash and loading to update info about it
+        window.close();
+      });
+  };
 
-	get options() {
-		if (this.state.accounts) {
-			return this.state.accounts.map((account, idx) => {
-				return this.getOption(account);
-			});
-		}
+  public onReject = () => {
+    window.close();
+  };
 
-		return [];
-	}
+  get options() {
+    if (this.state.accounts) {
+      return this.state.accounts.map((account, idx) => {
+        return this.getOption(account);
+      });
+    }
 
-	get amount() {
-		const { tx, blockchain } = this.state;
+    return [];
+  }
 
-		if (blockchain === networks.BTC.sign) {
-			return `${tx.amount / 1e8} BTC`;
-		}
-		if (blockchain === networks.ETH.sign) {
-			return `${web3.utils.fromWei(tx.value, 'ether')} ETH`;
-		}
+  get amount() {
+    const { tx, blockchain } = this.state;
 
-		return null;
-	}
+    if (blockchain === networks.BTC.sign) {
+      return `${tx.amount / 1e8} BTC`;
+    }
+    if (blockchain === networks.ETH.sign) {
+      return `${web3.utils.fromWei(tx.value, 'ether')} ETH`;
+    }
 
-	get gasControls() {
-		const gasPrice = web3.utils.fromWei(web3.utils.hexToNumber(this.state.tx.gasPrice).toString(), 'gwei');
-		const gasLimit = web3.utils.hexToNumber(this.state.tx.gasLimit);
-		const handlePrice = e => this.handleUpdateTX('gasPrice', e);
-		const handleLimit = e => this.handleUpdateTX('gasLimit', e);
+    return null;
+  }
 
-		return (
-			<React.Fragment>
-				<Control label="Gas Price:" value={gasPrice} onChange={handlePrice} secondLabel="gwei" />
-				<Control label="Gas Limit:" value={gasLimit} onChange={handleLimit} />
-			</React.Fragment>
-		);
-	}
+  get gasControls() {
+    const gasPrice = web3.utils.fromWei(web3.utils.hexToNumber(this.state.tx.gasPrice).toString(), 'gwei');
+    const gasLimit = web3.utils.hexToNumber(this.state.tx.gasLimit);
+    const handlePrice = e => this.handleUpdateTX('gasPrice', e);
+    const handleLimit = e => this.handleUpdateTX('gasLimit', e);
 
-	public handleUpdateTX(field, e) {
-		const value = e && e.target ? e.target.value : e;
-		const getValue = (field, value = 0) => {
-			switch (field) {
-				case 'gasPrice':
-					return web3.utils.toHex(web3.utils.toWei(value.toString(), 'gwei'));
-				case 'gasLimit':
-					return web3.utils.toHex(value.toString());
-			}
-		};
-		const hexValue = getValue(field, value);
+    return (
+      <React.Fragment>
+        <Control label="Gas Price:" value={gasPrice} onChange={handlePrice} secondLabel="gwei" />
+        <Control label="Gas Limit:" value={gasLimit} onChange={handleLimit} />
+      </React.Fragment>
+    );
+  }
 
-		this.setState(state => ({
-			...state,
-			tx: {
-				...state.tx,
-				[field]: hexValue
-			}
-		}));
-	}
+  public handleUpdateTX(field, e) {
+    const value = e && e.target ? e.target.value : e;
+    const getValue = (field, value = 0) => {
+      switch (field) {
+        case 'gasPrice':
+          return web3.utils.toHex(web3.utils.toWei(value.toString(), 'gwei'));
+        case 'gasLimit':
+          return web3.utils.toHex(value.toString());
+      }
+    };
+    const hexValue = getValue(field, value);
 
-	public render() {
-		// TODO: return loader
-		if (!this.state.isLoaded) return null;
+    this.setState(state => ({
+      ...state,
+      tx: {
+        ...state.tx,
+        [field]: hexValue
+      }
+    }));
+  }
 
-		// TODO: style this caption
-		if (!this.state.isReady || !this.state.tx) {
-			return (
-				<DialogLayout>
-					<Typography color="main">You need to authorize</Typography>
-				</DialogLayout>
-			);
-		}
+  private getAmountInUsd() {
+    const {
+      account: {
+        blockchain,
+      },
+      prices,
+      tx: { amount, value },
+    } = this.state;
+    let amountInCents = null;
 
-		const {
-			account: {
-				info: { address, balance },
-				blockchain,
-				id
-			},
-			tx: { to, amount, data },
-			tx,
-			selectValue
-		} = this.state;
-		const isEth = blockchain === networks.ETH.sign;
+    if (blockchain === 'BTC') {
+      amountInCents = parseInt(amount) / 1e8;
+    }
 
-		console.log('tx', tx);
+    if (blockchain === 'ETH') {
+      amountInCents = parseInt(value) / 1e18;
+    }
+    
+    if (prices && prices[blockchain]) {
+      return (prices[blockchain].USD * amountInCents).toFixed(2);
+    }
+  }
 
-		return (
-			<DialogLayout>
-				<div
-					className={css`
+  public render() {
+    // TODO: return loader
+    if (!this.state.isLoaded) return null;
+
+    // TODO: style this caption
+    if (!this.state.isReady || !this.state.tx) {
+      return (
+        <DialogLayout>
+          <Typography color="main">You need to authorize</Typography>
+        </DialogLayout>
+      );
+    }
+
+    const {
+      account: {
+        info: { address, balance },
+        blockchain,
+        id
+      },
+      tx: { to, amount, data },
+      tx,
+      selectValue
+    } = this.state;
+    const isEth = blockchain === networks.ETH.sign;
+
+    console.log(this.state);
+    console.log('tx', tx);
+
+    return (
+      <DialogLayout>
+        <div
+          className={css`
             display: flex;
             flex-direction: column;
           `}
-				>
-					<Typography
-						className={css`
+        >
+          <Typography
+            className={css`
               margin-right: 10px;
             `}
-						color="main"
-						variant="subheading"
-					>
-						Select wallet:
-					</Typography>
-					<Select options={this.options} onChange={value => this.handleChooseAccount(value)} value={selectValue} />
-				</div>
-				<Typography color="main" variant="subheading">
-					Send TX with params:
-				</Typography>
-				<Info label="From:" content={[address, `${balance} ${blockchain}`]} />
-				<Info label="To:" content={to} />
-				<Divider />
-				<Info label="Amount:" center content={[this.amount]} />
-				<Divider />
-				{isEth && this.gasControls}
-				{data && <Info label="Data:" content={data} />}
-				<Actions>
-					<Button large outlined onClick={this.onReject}>
-						Reject
-					</Button>
-					<Button large onClick={this.onSubmit}>
-						Submit
-					</Button>
-				</Actions>
-			</DialogLayout>
-		);
-	}
+            color="main"
+            variant="subheading"
+          >
+            Select wallet:
+          </Typography>
+          <Select options={this.options} onChange={value => this.handleChooseAccount(value)} value={selectValue} />
+        </div>
+        <Typography color="main" variant="subheading">
+          Send TX with params:
+        </Typography>
+        <Info label="From:" content={[address, `${balance} ${blockchain}`]} />
+        <Info label="To:" content={to} />
+        <Divider />
+        <Info label="Amount:" center content={[this.amount, `${this.getAmountInUsd()} USD`]} />
+        <Divider />
+        {isEth && this.gasControls}
+        {data && <Info label="Data:" content={data} />}
+        <Actions>
+          <Button large outlined onClick={this.onReject}>
+            Reject
+          </Button>
+          <Button large onClick={this.onSubmit}>
+            Submit
+          </Button>
+        </Actions>
+      </DialogLayout>
+    );
+  }
 }
