bf0a5deacb1ea4f678928913aa47f80bd9ed7ac9
mikeshukshin <merqrylive@gmail.com>
2018-09-16 19:23:31 +0300

    Add EOS in bc list

--- src/js/blockchain/index.ts
+++ src/js/blockchain/index.ts
@@ -33,5 +33,17 @@ export default {
         chainId: 4
       }
     ]
+  },
+  EOS: {
+    name: 'EOS',
+    sign: 'EOS',
+    network: [
+      {
+        name: 'Jungle Testnet',
+        sign: 'testnet',
+        chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+        url: 'http://193.93.219.219:8888'
+      }
+    ]
   }
 };
