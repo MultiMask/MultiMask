f7c48644ab1bb6fd58198797ee5b920716d56b40
mikeshukshin <merqrylive@gmail.com>
2018-10-24 19:31:45 +0300

    pretty code style

--- src/js/helpers/net.ts
+++ src/js/helpers/net.ts
@@ -2,7 +2,7 @@ export const strippedHost = (): string => {
   let host = location.hostname;
 
   // Replacing www. only if the domain starts with it.
-  if(host.indexOf('www.') === 0) host = host.replace('www.', '');
+  if(host.indexOf('www.') === 0) { host = host.replace('www.', ''); }
 
   return host;
 };
