57ed65d457c14f171482c1bdd93c621e1a00a7ec
mikeshukshin <merqrylive@gmail.com>
2018-11-15 18:15:48 +0300

    update profiles

--- src/js/helpers/profiles.ts
+++ src/js/helpers/profiles.ts
@@ -2,8 +2,7 @@ import sha256 from 'sha256';
 
 export const getParams = data => [data.substring(0,2), data.substring(2)];
 export const generateId = (bc, data): string => {
-  const type = data.slice(0,2);
-  const link = data.slice(2);
+  const [type, link] = getParams(data);
 
   if (type === '02') {
     return `${bc}${link}`;
